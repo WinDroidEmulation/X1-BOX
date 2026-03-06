bfc1b2ac0cabef24ccb0274814b6067540a2915b
izzy2lost <izzynochill@gmail.com>
2026-03-06 14:11:21 -0500

    bump version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
     minSdk = 26
     targetSdk = 36
 
-    versionCode = 15
-    versionName = "1.1.4"
+    versionCode = 16
+    versionName = "1.1.5"
 
     ndk {
       abiFilters += listOf("arm64-v8a")
